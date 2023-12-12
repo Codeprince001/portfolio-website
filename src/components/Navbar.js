ad1e7c5e23730fafa2bfebbf3875f8a50691f9c1
Codeprince001 <urangyorkwisdom@gmail.com>
2023-12-12 11:09:07 +0100

    update broken link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,11 +28,11 @@ export const NavBar = () => {
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""} >
       <Container>
-        <Navbar.Brand href="#home">Wisdom</Navbar.Brand>
+        <Navbar.Brand href="#">Wisdom</Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skill" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#project" className={activeLink === "projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span className='navbar-text'>
@@ -41,7 +41,7 @@ export const NavBar = () => {
               <a href='https://www.linkedin.com/in/wisdom-matthew-bb4818269/' className='linked-link' ><FontAwesomeIcon icon={faLinkedinIn} /></a>
               <a href='https://twitter.com/Kingswisdom001' ><FontAwesomeIcon icon={faTwitter} /></a>
             </div>
-            <button className='vvd' onClick={() => { console.log("Lets connect"); }}><span>Let's connect</span></button>
+            <a href='connect' style={{ color: "inherit", textDecoration: "none" }}><button className='connect-btn' onClick={() => onUpdateActiveLink("connect")}><span>Let's connect</span></button></a>
           </span>
         </Navbar.Collapse>
       </Container>
